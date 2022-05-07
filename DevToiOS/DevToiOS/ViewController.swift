2f58253f3fd80e0f209d73c06d7e5561b1a43e1d
Anton Paliakou <toxa95401@gmail.com>
2022-05-07 03:03:42 +0200

    Init project.

---
+++ DevToiOS/DevToiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevToiOS
+//
+//  Created by AntonPaliakou on 07/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
